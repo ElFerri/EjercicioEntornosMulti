a6be14392e6bd308966ec98f69684047c9796abc
ElFerri <play3ferran@gmail.com>
2022-02-03 19:03:24 +0100

    Modificacions

---
+++ EjercicioEntornosMulti/src/ejercicioentornosmulti/EjercicioEntornosMulti.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioentornosmulti;
+
+/**
+ *
+ * @author fersan
+ */
+public class EjercicioEntornosMulti {
+   /**
+     * Método multiplica para multiplicar los numeros
+     *
+     * @param a Simboliza un numero que podra ser escrito por el usuario
+     * @param b Simboliza el numero escrito por el usuario
+     * @return Devuelve el numero multiplicado
+     */
+    
+    public static int multiplica(int a, int b) {
+        return a*b;
+    
+}
+     /**
+     * Método main
+     * @param args Argumentos linia de comandos
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
