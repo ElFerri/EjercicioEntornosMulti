2009c52e445bebc2f09ec9c7a8b3c09eb0aa0d58
ElFerri <play3ferran@gmail.com>
2022-02-10 17:36:22 +0100

    Modificacions

---
+++ EjercicioEntornosMulti/test/ejercicioentornosmulti/EjercicioEntornosMultiTest.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package ejercicioentornosmulti;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author fersan
+ */
+public class EjercicioEntornosMultiTest {
+    
+    public EjercicioEntornosMultiTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of multiplica method, of class EjercicioEntornosMulti.
+     */
+    @Test
+    public void testMultiplica() {
+        System.out.println("multiplica");
+        int a = 0;
+        int b = 0;
+        int expResult = 0;
+        int result = EjercicioEntornosMulti.multiplica(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class EjercicioEntornosMulti.
+     */
+    @Ignore
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        EjercicioEntornosMulti.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
